a8a030d7caff3d67a23f1a0545b9e6a844201c9b
WILL <wsleal@yahoo.com>
2022-04-28 11:23:41 -0300

    .

---
+++ Java/MeuPrimeiroProgramaJava.java
@@ -0,0 +1,14 @@
+class MeuPrimeiroProgramaJava {
+    public static void main(String entrada[]) {
+        int inteiro = 47;
+        // char caracter = 'F';
+        double real = 1.65;
+        String frase = "Lucy Mari";
+        boolean VF = true;
+
+        if (VF == true) {
+            System.out.println("Eu sou " + frase + " tenho " + inteiro + " anos, e tenho " + real + " m de altura");
+        }
+        System.exit(0);
+    }
+}
