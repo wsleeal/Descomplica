9b0b3cf02f6d695fd84f4fb167fe9aac0d956cc8
Your Name <you@example.com>
2022-04-27 21:36:18 -0300

    .

---
+++ Java/MeuPrimeiroProgramaJava.java
@@ -0,0 +1,13 @@
+
+class MeuPrimeiroProgramaJava {
+    public static void main(String entrada[]) {
+        // Entrada de dados
+        int n1 = Integer.parseInt(entrada[0]);
+        int n2 = Integer.parseInt(entrada[1]);
+        // Processamento
+        int soma = n1 + n2;
+        // Saida de resultados
+        System.out.println(n1 + " + " + n2 + " = " + soma);
+        System.exit(0);
+    }
+}
