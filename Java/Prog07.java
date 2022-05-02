23afc75762562f39c1eba519ac0626bc79d09b68
Your Name <you@example.com>
2022-05-02 13:58:38 -0300

    .

---
+++ Java/Prog07.java
@@ -0,0 +1,29 @@
+import javax.swing.JOptionPane;
+
+class Prog07 {
+
+    public static void soma() {
+        int s = 0, vet[] = { 1, 2, 3, 4, 5 };
+        for (int i = 0; i < vet.length; i++) {
+            s = s + vet[i];
+        }
+        JOptionPane.showMessageDialog(null, "A somatoria eh " + s);
+    }
+
+    public static int produto() {
+        int p = 1, vet[] = { 1, 2, 3, 4, 5 };
+        for (int i = 0; i < vet.length; i++) {
+            p = p * vet[i];
+        }
+        return p;
+    }
+
+    public static void main(String[] args) {
+
+        int r;
+        soma();
+        r = produto();
+        JOptionPane.showMessageDialog(null, "A produtoria eh " + r);
+        System.exit(0);
+    }
+}
